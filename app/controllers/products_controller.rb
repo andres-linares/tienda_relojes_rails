d06cc248bea7cae6dc502091d6ce4ca3d9b579b8
andres-linares <andres.linares.971122@gmail.com>
2019-05-28 10:16:37 -0500

    Configuring a new gem for source changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,18 @@ class ProductsController < ApplicationController
     def index
 
     end
+
+    def new
+
+    end
+
+    def create
+      puts params
+      @product = Product.new(product_params)
+    end
+
+    private
+    def product_params
+      params.require(:product).permit(:name, :description, :price, images: [])
+    end
 end
