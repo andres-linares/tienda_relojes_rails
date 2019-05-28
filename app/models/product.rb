55249d4b6674b2f5ee63685625315cc174f7519f
andres-linares <andres.linares.971122@gmail.com>
2019-05-28 18:01:48 -0500

    updating seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many_attached :images
 
+  belongs_to :brands
+
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Brand.create([
+  { name: 'Rollex' },
+  { name: 'Armani' },
+  { name: 'Fossil' },
+  { name: 'Tissot' },
+  { name: 'Wenger' },
+  { name: 'Casio' },
+  { name: 'Invicta' },
+  { name: 'BMW' },
+  { name: 'Tommy Hilfiger' },
+  { name: 'Q&Q' },
+  { name: 'Calvin Klein' },
+  { name: 'Diesel' },
+  { name: 'Certina' },
+  { name: 'G-Shock' },
+  { name: 'Michael Kors' },
+])
+
 product_1 = Product.create(
-  name: 'Rolex Hombre GMT (nueva colección)',
-  description: 'El diseño robusto y funcional del Oyster Perpetual se convirtió rapidamente en un icono.',
-  price: 59500
+  name: 'T-Race',
+  description: 'El Tissot T-Race es un ganador dentro y fuera del circuito. Los detalles de diseño inspirados en las carreras de bicicletas y el estilo dinámico, con una pequeña dosis de colores extravagantes, ponen a estos modelos en marcha y les da una posición inicial igualmente fuerte. Con Tissot T-Race, la pasión de Tissot por el deporte del motor se está acelerando en el carril rápido de la moda.',
+  price: 2706000,
+  brand_id: Brand.find_by_name('Tissot').id
 )
-product_1.images.attach(io: File.open('clock_images/1/1.png'), filename: '1.png')
-product_1.images.attach(io: File.open('clock_images/1/2.png'), filename: '2.png')
-product_1.images.attach(io: File.open('clock_images/1/3.png'), filename: '3.png')
-
-product_2 = Product.create(
-  name: 'Winner Skeleton Automatico 246',
-  description: 'Winner watch, nueva llegada de relojes para hombre, inspirado en el mejor estilo actual, movimiento automático de gran precisión.',
-  price: 89900
-)
-product_2.images.attach(io: File.open('clock_images/2/1.png'), filename: '1.png')
-product_2.images.attach(io: File.open('clock_images/2/2.png'), filename: '2.png')
-product_2.images.attach(io: File.open('clock_images/2/3.png'), filename: '3.png')
-
-product_3 = Product.create(
-  name: 'Invicta Reserve',
-  description: 'Invicta de acero inoxidable creado y manufacturado en Suiza.',
-  price: 2559900
-)
-product_3.images.attach(io: File.open('clock_images/3/1.png'), filename: '1.png')
-product_3.images.attach(io: File.open('clock_images/3/2.png'), filename: '2.png')
-product_3.images.attach(io: File.open('clock_images/3/3.png'), filename: '3.png')
-
-product_4 = Product.create(
-  name: 'Fossil Gen 3 Q',
-  description: 'Reloj elegante Fossil para hombres Gen 3 Q Explorist Luggage Leather',
-  price: 878990
-)
-product_4.images.attach(io: File.open('clock_images/4/1.png'), filename: '1.png')
-product_4.images.attach(io: File.open('clock_images/4/2.png'), filename: '2.png')
-product_4.images.attach(io: File.open('clock_images/4/3.png'), filename: '3.png')
+product_1.images.attach(io: File.open('clock_images/1/1.jpg'), filename: '1.jpg')
+product_1.images.attach(io: File.open('clock_images/1/2.jpg'), filename: '2.jpg')
+product_1.images.attach(io: File.open('clock_images/1/3.jpg'), filename: '3.jpg')
