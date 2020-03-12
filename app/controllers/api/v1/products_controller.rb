3ba6a2ddea9b9437ced51ab4cb203fb0308b8ac3
andres-linares <andres.linares.971122@gmail.com>
2020-03-12 06:13:04 -0500

    added API

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class ProductsController < ApiController
+
+      def index
+        @products = Product.all
+        render json: @products
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   resources :products do
     resources :purchases, only: [:index, :new, :create]
   end
+
+  namespace :api, constraints: { format: 'json' } do
+    namespace :v1 do
+      resources :products, only: [:index]
+    end
+  end
 end
